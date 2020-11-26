757f342b9421a1fda75fcda8462e708f30f28794
mohanmohadikar <mohadikar.mohan@sitpune.edu.in>
2020-11-26 19:52:01 +0530

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -120,7 +120,7 @@ def success():
 				output.close()
 				img = filename
 
-				res = pred(img)
+				res = pred(img_path)
 
 				#class_result , prob_result = predict(img_path , model)
 
@@ -150,7 +150,7 @@ def success():
 				file.save(os.path.join(target_img , file.filename))
 				img_path = os.path.join(target_img , file.filename)
 				img = file.filename
-				res = pred(img)
+				res = pred(img_path)
 
 				#class_result , prob_result = predict(img_path , model)
 			#really nigga.
