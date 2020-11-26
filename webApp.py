dd7df47b51de623b1969cd4662e3ed7959d6475b
mohanmohadikar <mohadikar.mohan@sitpune.edu.in>
2020-11-26 19:01:00 +0530

    Update webApp.py

--- webApp.py
+++ webApp.py
@@ -31,11 +31,11 @@ from tensorflow.keras.preprocessing.image import load_img , img_to_array
 
 app = Flask(__name__)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-#model = load_model(os.path.join(BASE_DIR , 'ModelWebApp.hdf5'))
-model = load_model("static/model.h5")
-joblib_file = open("static/tokenizer.p", "rb")
+model = load_model(os.path.join(BASE_DIR , 'ModelWebApp.hdf5'))
+#model = load_model("static/model.h5")
+#joblib_file = open("static/tokenizer.p", "rb")
 # Load from file
-tokenizer = pickle.load(joblib_file)
+#tokenizer = pickle.load(joblib_file)
 
 
 ALLOWED_EXT = set(['jpg' , 'jpeg' , 'png' , 'jfif'])
@@ -96,12 +96,12 @@ def success():
 				output.close()
 				img = filename
 
-				res = pred(img_path)
+				#res = pred(img_path)
 
 				#class_result , prob_result = predict(img_path , model)
 
 				predictions = {
-					  "class1":res,
+					  "class1":"res",
 					    "class2":"class_result[1]",
 					    "class3":"class_result[2]",
 					    "prob1": "prob_result[0]",
@@ -128,10 +128,10 @@ def success():
 				img = file.filename
 				res = pred(img_path)
 
-			#	class_result , prob_result = predict(img_path , model)
+				#class_result , prob_result = predict(img_path , model)
 			#really nigga.
 				predictions = {
-					  "class1":res,
+					  "class1":"res",
 					    "class2":"class_result[1]",
 					    "class3":"class_result[2]",
 					    "prob1": "prob_result[0]",
@@ -155,59 +155,6 @@ def success():
 
 
 
-
-def extract_features(filename, model):
-    try:
-        image = Image.open(filename)
-            
-    except:
-        print("ERROR: Couldn't open image! Make sure the image path and extension is correct")
-    image = image.resize((299,299))
-    image = np.array(image)
-    # for images that has 4 channels, we convert them into 3 channels
-    if image.shape[2] == 4: 
-        image = image[..., :3]
-    image = np.expand_dims(image, axis=0)
-    image = image/127.5
-    image = image - 1.0
-    feature = model.predict(image)
-    return feature
-
-def word_for_id(integer, tokenizer):
- 	for word, index in tokenizer.word_index.items():
-    	 if index == integer:
-        	return word
- 	return None
-
-
-def generate_desc(model, tokenizer, photo, max_length):
-    in_text = 'start'
-    for i in range(max_length):
-        sequence = tokenizer.texts_to_sequences([in_text])[0]
-        sequence = pad_sequences([sequence], maxlen=max_length)
-        pred = model.predict([photo,sequence], verbose=0)
-        pred = np.argmax(pred)
-        word = word_for_id(pred, tokenizer)
-        if word is None:
-            break
-        in_text += ' ' + word
-        if word == 'end':
-            break
-    return in_text
-
-
-def pred(img_path):
-	max_length = 32
-#	tokenizer = pickle.load(open("static/tokenizer.p","rb"))
-#	model = load_model(os.path.join(BASE_DIR , 'model.h5'))
-	#model = load_model('models/model_9.h5')
-	xception_model = Xception(include_top=False, pooling="avg")
-
-	photo = extract_features(img_path, xception_model)
-	img = Image.open(img_path)
-
-	description = generate_desc(model, tokenizer, photo, max_length)
-	return description
 
 
 
